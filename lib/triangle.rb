453929ecb1ebbf089014e19feec910c4a1f2bfe4
meaghan jennings <meaghanljennings@gmail.com>
2020-10-23 00:36:00 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -20,9 +20,9 @@ class Triangle
   end
   
   def kind
-   if side_1 == side_2 && side_1 == side_3
+   if @side_1 == @side_2 && @side_1 == @side_3
       return :equilateral
-    elsif side_1 == side_2 || side_2 == side_3 || side_3 == side_1
+    elsif @side_1 == @side_2 || @side_2 == @side_3 || @side_3 == @side_1
       return :isosceles
     else
       return :scalene
