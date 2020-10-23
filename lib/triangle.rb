b86e26c791267bf66d87179fac70069a7a738851
meaghan jennings <meaghanljennings@gmail.com>
2020-10-23 00:30:28 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -4,6 +4,19 @@ class Triangle
     @side_1 = side_1
     @side_2 = side_2
     @side_3 = side_3
+    
+    if side_1 == 0 || side_2 == 0 || side_3 == 0
+      raise TriangleError
+    end
+  
+    if side_1 <= 0 || side_2 <= 0 || side_3 <= 0
+      raise TriangleError
+    end
+    
+    if side_1 >= (side_2 + side_3) || side_2 >= (side_1 + side_3) || side_3 >= (side_1 + side_2)
+      raise TriangleError
+    end
+    
   end
   
   def kind()
